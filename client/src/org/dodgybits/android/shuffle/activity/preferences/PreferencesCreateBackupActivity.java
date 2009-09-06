b0cdb4eafaeaa78476bfdf4f2964f6a0a9867401
andybryant <andybryant@c433d9f2-e643-0410-a449-b9b772e888ec>
2009-09-06 16:27:05 +0000

    Cleaned up UI when creating backup (still work in progress)
    git-svn-id: http://android-shuffle.googlecode.com/svn/trunk@78 c433d9f2-e643-0410-a449-b9b772e888ec

--- client/src/org/dodgybits/android/shuffle/activity/preferences/PreferencesCreateBackupActivity.java
+++ client/src/org/dodgybits/android/shuffle/activity/preferences/PreferencesCreateBackupActivity.java
@@ -18,20 +18,29 @@ import org.dodgybits.shuffle.dto.ShuffleProtos.Catalogue.Builder;
 
 import android.app.Activity;
 import android.database.Cursor;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class PreferencesCreateBackupActivity extends Activity 
 	implements View.OnClickListener {
     private static final String cTag = "PreferencesCreateBackupActivity";
     
     private EditText mFilenameWidget;
-
+    private Button mSaveButton;
+    private Button mCancelButton;
+    private ProgressBar mProgressBar;
+    private TextView mProgressText;
+    
+    private AsyncTask<?, ?, ?> mTask;
+    
     @Override
     protected void onCreate(Bundle icicle) {
         Log.d(cTag, "onCreate+");
@@ -40,11 +49,19 @@ public class PreferencesCreateBackupActivity extends Activity
 
         setContentView(R.layout.backup_create);
         
+        mProgressBar = (ProgressBar) findViewById(R.id.progress_horizontal);
+        mProgressBar.setVisibility(View.INVISIBLE);
+        mProgressText = (TextView) findViewById(R.id.progress_label);
+        mSaveButton = (Button) findViewById(R.id.saveButton);
+        mCancelButton = (Button) findViewById(R.id.discardButton);
+        mCancelButton.setText(R.string.cancel_button_title);
+        
         mFilenameWidget = (EditText) findViewById(R.id.filename);
         Date today = new Date();
         SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMdd");
         String defaultText = "shuffle-" + formatter.format(today) + ".bak";
         mFilenameWidget.setText(defaultText);
+        mFilenameWidget.setSelection(0, defaultText.length() - 4);
         
         addSavePanelListeners();
     }
@@ -53,12 +70,21 @@ public class PreferencesCreateBackupActivity extends Activity
         switch (v.getId()) {
 
             case R.id.saveButton:
+            	setControlsEnabled(false);
             	createBackup();
                 break;
 
             case R.id.discardButton:
             	finish();
                 break;
+        }
+    }
+    
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mTask != null && mTask.getStatus() != AsyncTask.Status.RUNNING) {
+            mTask.cancel(true);
         }
     }
 
@@ -70,6 +96,18 @@ public class PreferencesCreateBackupActivity extends Activity
         view.setOnClickListener(this);
     }
     
+    private void setControlsEnabled(boolean enabled) {
+    	mFilenameWidget.setEnabled(enabled);
+    	mSaveButton.setEnabled(enabled);
+    	mCancelButton.setEnabled(enabled);
+    }
+    
+    private void showOkButton() {
+    	mSaveButton.setVisibility(View.GONE);
+    	mCancelButton.setText(R.string.ok_button_title);
+    	mCancelButton.setEnabled(true);
+    }
+    
     private void createBackup() {
     	String filename = mFilenameWidget.getText().toString();
     	if (TextUtils.isEmpty(filename)) {
@@ -78,6 +116,7 @@ public class PreferencesCreateBackupActivity extends Activity
 			return;
     	} 
     	
+    	Log.d(cTag, "Checking media state");
     	String storage_state = Environment.getExternalStorageState();
     	if (! Environment.MEDIA_MOUNTED.equals(storage_state)) {
     		// TODO show alert
@@ -88,56 +127,134 @@ public class PreferencesCreateBackupActivity extends Activity
 		File dir = Environment.getExternalStorageDirectory();
 		File backupFile = new File(dir, filename);
 		try {
+	    	Log.d(cTag, "Creating backup file");
 			backupFile.createNewFile();
 			FileOutputStream out = new FileOutputStream(backupFile);
-			writeBackup(out);
+			mTask = new CreateBackupTask().execute(out);
 		} catch (IOException ioe) {
 			// TODO show alert
 			Log.e(cTag, "Failed to create backup " + ioe.getMessage());
 			return;
 		}
-    	
-		String text = getResources().getString(R.string.toast_backup_saved, filename);
-        Toast.makeText(this, text, Toast.LENGTH_LONG).show();
-        finish();
     }
     
-    private void writeBackup(OutputStream out) throws IOException {
-    	// TODO display progress
-    	Builder builder = Catalogue.newBuilder();
+    private class CreateBackupTask extends AsyncTask<OutputStream, CreateBackupProgress, Void> {
+
+    	public Void doInBackground(OutputStream... out) {
+            try {
+            	writeBackup(out[0]);
+            } catch (Exception e) {
+            	String message = "Backup failed " + e.getMessage();
+            	updateProgress(100, message, true);
+            }
+            
+            return null;
+        }
     	
-    	// write contexts
-        Cursor contextCursor = getContentResolver().query(
-        		Shuffle.Contexts.CONTENT_URI, Shuffle.Contexts.cFullProjection, 
-        		null, null, null);
-    	while (contextCursor.moveToNext()) {
-    		Context context = BindingUtils.readContext(contextCursor, getResources());
-        	builder.addContext(context.toDto());
-    	}
-    	contextCursor.close();
+        private void writeBackup(OutputStream out) throws IOException {
+        	Builder builder = Catalogue.newBuilder();
+        	
+        	writeContexts(builder, 0, 15);
+        	writeProjects(builder, 15, 30);
+        	writeTasks(builder, 30, 100);
+
+        	builder.build().writeTo(out);
+        	out.close();
+
+        	String message = "Backup complete.";
+        	updateProgress(100, message, false);
+        }
+        
+        private void writeContexts(Builder builder, int progressStart, int progressEnd)
+        {
+	    	Log.d(cTag, "Writing contexts");
+            Cursor cursor = getContentResolver().query(
+            		Shuffle.Contexts.CONTENT_URI, Shuffle.Contexts.cFullProjection, 
+            		null, null, null);
+            int i = 0;
+            int total = cursor.getCount();
+            String type = getString(R.string.context_name);
+        	while (cursor.moveToNext()) {
+        		Context context = BindingUtils.readContext(cursor, getResources());
+            	builder.addContext(context.toDto());
+    			String text = getString(R.string.backup_progress, type, context.name);
+    			int percent = calculatePercent(progressStart, progressEnd, ++i, total);
+            	updateProgress(percent, text, false);
+        	}
+        	cursor.close();
+        }
+
+        private void writeProjects(Builder builder, int progressStart, int progressEnd)
+        {
+	    	Log.d(cTag, "Writing projects");
+            Cursor cursor = getContentResolver().query(
+            		Shuffle.Projects.CONTENT_URI, Shuffle.Projects.cFullProjection, 
+            		null, null, null);
+            int i = 0;
+            int total = cursor.getCount();
+            String type = getString(R.string.project_name);
+        	while (cursor.moveToNext()) {
+        		Project project = BindingUtils.readProject(cursor);
+            	builder.addProject(project.toDto());
+    			String text = getString(R.string.backup_progress, type, project.name);
+    			int percent = calculatePercent(progressStart, progressEnd, ++i, total);
+            	updateProgress(percent, text, false);
+        	}
+        	cursor.close();
+        }
+        
+        private void writeTasks(Builder builder, int progressStart, int progressEnd)
+        {
+	    	Log.d(cTag, "Writing tasks");
+            Cursor cursor = getContentResolver().query(
+            		Shuffle.Tasks.CONTENT_URI, Shuffle.Tasks.cExpandedProjection, 
+            		null, null, null);
+            int i = 0;
+            int total = cursor.getCount();
+            String type = getString(R.string.task_name);
+        	while (cursor.moveToNext()) {
+        		Task task = BindingUtils.readTask(cursor, getResources());
+            	builder.addTask(task.toDto());
+    			String text = getString(R.string.backup_progress, type, task.description);
+    			int percent = calculatePercent(progressStart, progressEnd, ++i, total);
+            	updateProgress(percent, text, false);
+        	}
+        	cursor.close();
+        }
+        
+        private int calculatePercent(int start, int end, int current, int total) {
+        	return start + (end - start) * current / total;
+        }
+        
+        private void updateProgress(int progressPercent, String details, boolean isError) {
+        	CreateBackupProgress progress = new CreateBackupProgress();
+        	progress.progressPercent = progressPercent;
+        	progress.details = details;
+        	progress.isError = isError;
+        	publishProgress(progress);
+        }
+        
+		@Override
+		public void onProgressUpdate (CreateBackupProgress... progresses) {
+			CreateBackupProgress progress = progresses[0];
+	        mProgressBar.setVisibility(View.VISIBLE);
+	        mProgressBar.setProgress(progress.progressPercent);
+	        mProgressText.setText(progress.details);
+	        if (progress.progressPercent == 100) {
+	            showOkButton();
+	        }
+		}
+		
+        public void onPostExecute() {
+            mTask = null;
+        }
     	
-    	// write projects
-        Cursor projectCursor = getContentResolver().query(
-        		Shuffle.Projects.CONTENT_URI, Shuffle.Projects.cFullProjection, 
-        		null, null, null);
-    	while (projectCursor.moveToNext()) {
-    		Project project = BindingUtils.readProject(projectCursor);
-        	builder.addProject(project.toDto());
-    	}
-    	projectCursor.close();
-    	
-    	// write tasks
-        Cursor taskCursor = getContentResolver().query(
-        		Shuffle.Tasks.CONTENT_URI, Shuffle.Tasks.cExpandedProjection, 
-        		null, null, null);
-    	while (taskCursor.moveToNext()) {
-    		Task task = BindingUtils.readTask(taskCursor, getResources());
-        	builder.addTask(task.toDto());
-    	}
-    	taskCursor.close();
-    	
-    	builder.build().writeTo(out);
-    	out.close();
+    }
+    
+    private class CreateBackupProgress {
+    	public int progressPercent;
+    	public String details;
+    	public boolean isError;
     }
 
 }
