a6d7bd89aed3a0fcc01735b64855aaf326ede233
andybryant <andybryant@c433d9f2-e643-0410-a449-b9b772e888ec>
2010-02-16 22:56:57 +0000

    Added start of task query work
    git-svn-id: http://android-shuffle.googlecode.com/svn/trunk@158 c433d9f2-e643-0410-a449-b9b772e888ec

--- client/src/org/dodgybits/shuffle/android/core/model/persistence/Query.java
+++
@@ -1,5 +0,0 @@
-package org.dodgybits.shuffle.android.core.model.persistence;
-
-public interface Query {
-
-}
---
+++ client/src/org/dodgybits/shuffle/android/core/model/persistence/TaskQuery.java
@@ -0,0 +1,14 @@
+package org.dodgybits.shuffle.android.core.model.persistence;
+
+public interface TaskQuery {
+
+	/**
+	 * projects- explicit list, none or ignore
+	 * contexts - explicit list, none or ignore
+	 * top or all per context
+	 * top or all per project
+	 * start date range
+	 * due date range
+	 * created date range (for Inbox)
+	 */
+}
